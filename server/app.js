1c7da710800cbb288bed3aa9e51c7c8a52363e80
Orischn <orischn02@gmail.com>
2023-06-02 17:44:11 +0300

    implemented basic websockets on the server side

--- server/app.js
+++ server/app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
+const http = require('http')
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
+let sockets = [];
 
 app.use(express.json());
 app.use(express.static('public'));
@@ -7,4 +12,15 @@ app.use(require('./routes/token'));
 app.use(require('./routes/users'));
 app.use(require('./routes/chats'));
 app.use(require('./routes/messages'));
+
+io.on('connection', (socket) => {
+    // socket.on('loggedIn', (data) => {
+    //     socket.name=data.username;
+    //     sockets[data.username] = socket.id
+    // })
+    socket.on('messageSent', (message) => {
+        socket.emit('message', message);
+    })
+}) 
+
 app.listen(5000);
