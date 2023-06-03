98e3ec48d9c71b85f98f39384c31d147993ae2c1
NadavRam1 <nadavr2005@gmail.com>
2023-06-03 14:16:05 +0300

    Did some modifications

--- server/app.js
+++ server/app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const http = require('http')
 const server = http.createServer(app);
-const { Server } = require('socket.io');
-const io = new Server(server);
-let sockets = [];
+// const { Server } = require('socket.io');
+// const io = new Server(server);
+// let sockets = [];
 
 app.use(express.json());
 app.use(express.static('public'));
@@ -13,14 +13,14 @@ app.use(require('./routes/users'));
 app.use(require('./routes/chats'));
 app.use(require('./routes/messages'));
 
-io.on('connection', (socket) => {
-    // socket.on('loggedIn', (data) => {
-    //     socket.name=data.username;
-    //     sockets[data.username] = socket.id
-    // })
-    socket.on('messageSent', (message) => {
-        socket.emit('message', message);
-    })
-}) 
+// io.on('connection', (socket) => {
+//     // socket.on('loggedIn', (data) => {
+//     //     socket.name=data.username;
+//     //     sockets[data.username] = socket.id
+//     // })
+//     socket.on('messageSent', (message) => {
+//         socket.emit('message', message);
+//     })
+// }) 
 
 app.listen(5000);
